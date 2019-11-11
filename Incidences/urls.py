94740b244856a2c65c0a3cd60af8e41b992d3e17
MizanUrp11 <mrn1105009@gmail.com>
2019-11-11 23:12:24 +0600

    Adding point layer

--- Incidences/urls.py
+++ Incidences/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include,url
-from views import HomepageView, county_dataset
+from views import HomepageView, county_dataset,incidences_dataset
 
 urlpatterns = [
     url(r'^$', HomepageView.as_view(), name='home'),
     url(r'^county_data/', county_dataset, name='county'),
-    url(r'^incidence_data', HomepageView.as_view(), name='home'),
+    url(r'^incidence_data/', incidences_dataset, name='incidences'),
 
 
 ]
--- Incidences/views.py
+++ Incidences/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.http import HttpResponse
-from .models import Counties
+from .models import Counties,ReporterModel
 # Create your views here.
 
 class HomepageView(TemplateView):
@@ -11,3 +11,7 @@ class HomepageView(TemplateView):
 def county_dataset(request):
     counties = serialize('geojson', Counties.objects.all())
     return HttpResponse(counties,content_type='json')
+
+def incidences_dataset(request):
+    incidences = serialize('geojson', ReporterModel.objects.all())
+    return HttpResponse(incidences,content_type='json')
